592122d735ff3c74be4e3e251c4135ef8a7f3c76
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-20 10:17:22 +0800

    Handle clean workspace.

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,34 +1,44 @@
 <template>
   <div class="box">
-    <div class="unstaged-title">
-      Unstaged changes ({{ unstageStatusInfo.length }})
-    </div>
     <div
-      v-for="info in unstageStatusInfo"
-      :key="info"
-      class="item">
-      <div class="type">
-        {{ info.type }}
-      </div>
-      <div class="file">
-        {{ info.file }} 
-      </div>
+      v-if="noFileSubmit()"
+      class="clean-workspace">
+      No file need submitted, clean workspace.
     </div>
-    
-    <br/>
-    
-    <div class="staged-title">
-      Staged changes ({{ stageStatusInfo.length }})
-    </div>
-    <div
-      v-for="info in stageStatusInfo"
-      :key="info"
-      class="item">
-      <div class="type">
-        {{ info.type }}
+    <div v-else>
+      <div v-if="unstageFileNumber() > 0">
+        <div class="unstaged-title">
+          Unstaged changes ({{ unstageStatusInfo.length }})
+        </div>
+        <div
+          v-for="info in unstageStatusInfo"
+          :key="info"
+          class="item">
+          <div class="type">
+            {{ info.type }}
+          </div>
+          <div class="file">
+            {{ info.file }}
+          </div>
+          <br/>
+        </div>
       </div>
-      <div class="file">
-        {{ info.file }} 
+
+      <div v-if="stageFileNumber() > 0">
+        <div class="staged-title">
+          Staged changes ({{ stageStatusInfo.length }})
+        </div>
+        <div
+          v-for="info in stageStatusInfo"
+          :key="info"
+          class="item">
+          <div class="type">
+            {{ info.type }}
+          </div>
+          <div class="file">
+            {{ info.file }}
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -40,6 +50,29 @@
    props: {
      stageStatusInfo: Array,
      unstageStatusInfo: Array
+   },
+   methods: {
+     noFileSubmit() {
+       return this.unstageFileNumber() + this.stageFileNumber() === 0;
+     },
+     
+     unstageFileNumber() {
+       var unstage_files_number = 0;
+       if (this.unstageStatusInfo) {
+         unstage_files_number = this.unstageStatusInfo.length;
+       }
+       
+       return unstage_files_number;
+     },
+
+     stageFileNumber() {
+       var stage_files_number = 0;
+       if (this.stageStatusInfo) {
+         stage_files_number = this.stageStatusInfo.length;
+       }
+       
+       return stage_files_number;
+     }
    }
  }
 </script>
@@ -50,21 +83,25 @@
    width: 100%;
    height: 100%;
  }
- 
+
  .item {
    padding-left: 20px;
    padding-right: 10px;
    padding-top: 2px;
    padding-bottom: 2px;
    font-size: 16px;
-   
+
    display: flex;
    flex-direction: row;
    align-items: center;
  }
- 
+
  .type {
    padding-right: 10px;
+ }
+
+ .clean-workspace {
+   padding-left: 10px;
  }
  
  .unstaged-title {
