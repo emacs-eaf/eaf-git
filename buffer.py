c6e118db201c289b774854b4656b71a9374aab6d
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-20 22:00:52 +0800

    Pretty commit date.

--- buffer.py
+++ buffer.py
@@ -63,6 +63,49 @@ GIT_STATUS_DICT = {
     GIT_STATUS_CONFLICTED: "Conflicted"
 }
 
+def pretty_date(time=False):
+    """
+    Get a datetime object or a int() Epoch timestamp and return a
+    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
+    'just now', etc
+    """
+    from datetime import datetime
+    now = datetime.now()
+    if type(time) is int:
+        diff = now - datetime.fromtimestamp(time)
+    elif isinstance(time, datetime):
+        diff = now - time
+    elif not time:
+        diff = 0
+    second_diff = diff.seconds
+    day_diff = diff.days
+
+    if day_diff < 0:
+        return ''
+
+    if day_diff == 0:
+        if second_diff < 10:
+            return "just now"
+        if second_diff < 60:
+            return str(second_diff) + " seconds ago"
+        if second_diff < 120:
+            return "a minute ago"
+        if second_diff < 3600:
+            return str(second_diff // 60) + " minutes ago"
+        if second_diff < 7200:
+            return "an hour ago"
+        if second_diff < 86400:
+            return str(second_diff // 3600) + " hours ago"
+    if day_diff == 1:
+        return "Yesterday"
+    if day_diff < 7:
+        return str(day_diff) + " days ago"
+    if day_diff < 31:
+        return str(day_diff // 7) + " weeks ago"
+    if day_diff < 365:
+        return str(day_diff // 30) + " months ago"
+    return str(day_diff // 365) + " years ago"
+
 class AppBuffer(BrowserBuffer):
     def __init__(self, buffer_id, url, arguments):
         BrowserBuffer.__init__(self, buffer_id, url, arguments, False)
@@ -202,7 +245,7 @@ class FetchLogThread(QThread):
         for commit in self.repo.walk(self.repo.head.target, GIT_SORT_TOPOLOGICAL):
             git_log.append({
                 "id": str(commit.id),
-                "time": datetime.utcfromtimestamp(int(commit.commit_time)).strftime('%Y-%m-%d %H:%M:%S'),
+                "time": pretty_date(int(commit.commit_time)),
                 "author": commit.author.name,
                 "message": commit.message.splitlines()[0]
             })
