2b335b74a6bed6d8a5f07dd80b0a7ee7ca760c9b
Andy Stewart <lazycat.manatee@gmail.com>
2022-03-29 15:05:52 +0800

    Port to PyQt6

--- buffer.py
+++ buffer.py
@@ -19,9 +19,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from PyQt5 import QtCore
-from PyQt5.QtGui import QColor
-from PyQt5.QtCore import QThread
+from PyQt6 import QtCore
+from PyQt6.QtGui import QColor
+from PyQt6.QtCore import QThread
 from core.webengine import BrowserBuffer
 from core.utils import get_emacs_func_result, get_emacs_var, PostGui, message_to_emacs, eval_in_emacs
 from pygit2 import (Repository, GIT_SORT_TOPOLOGICAL,
