87c070d128901fe32de09c37bd285da188a89aff
Andy Stewart <lazycat.manatee@gmail.com>
2022-05-13 20:50:38 +0800

    Use git command fetch root path.

--- generate_file_permalink.py
+++ generate_file_permalink.py
@@ -3,6 +3,21 @@ import sys
 from giturlparse import parse
 from pygit2 import Repository
 
+def get_command_result(command_string, cwd):
+    import subprocess
+    process = subprocess.Popen(command_string, cwd=cwd, shell=True, text=True,
+                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ret = process.wait()
+    return "".join((process.stdout if ret == 0 else process.stderr).readlines()).strip()
+
+def get_project_path(filepath):
+    import os
+    dir_path = os.path.dirname(filepath)
+    if get_command_result("git rev-parse --is-inside-work-tree", dir_path) == "true":
+        return get_command_result("git rev-parse --show-toplevel", dir_path)
+    else:
+        return filepath
+    
 def generate_file_permalink(file, line):
     repo = Repository(file)
     
@@ -11,11 +26,8 @@ def generate_file_permalink(file, line):
         origin_url = origin_url[:-len(".git")]
     
     head_tree = str(repo.head.target)
-    
-    repo_root = repo.path
-    if repo_root.endswith(".git/"):
-        repo_root = repo_root[:-len(".git/")]
-    
+    repo_root = get_project_path(file)
+        
     return "{}/blob/{}/{}#L{}".format(origin_url, head_tree, file.split(repo_root)[1], line)
 
 if __name__ == "__main__":
